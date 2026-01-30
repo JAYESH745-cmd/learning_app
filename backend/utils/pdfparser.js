4633fc6f10628a985cf9f627672c08db3cdc0088
Jayesh Arora <jayesharora0001@gmail.com>
2026-01-30 18:14:27 +0530

    change

--- backend/utils/pdfparser.js
+++ backend/utils/pdfparser.js
@@ -1,22 +1,19 @@
-import fs from "fs/promises"
-import { PDFParse } from "pdf-parse"
-import { fileURLToPath } from "url"
+import fs from "fs/promises";
+import pdf from "pdf-parse";
 
+export const extractTextFromPDF = async (filePath) => {
+  try {
+    const dataBuffer = await fs.readFile(filePath);
 
-export const extractTextFromPDF=async(filePath)=>{
-    try {
-        const dataBuffer=await fs.readFile(filePath);
-        const parser=new PDFParse(new Uint8Array(dataBuffer));
-        const data=await parser.getText();
+    const data = await pdf(dataBuffer);
 
-        return {
-        text:data.text,
-        numPages:data.numPages,
-        info:data.info
-        }
-
-    } catch (error) {
-        console.error("PDF parsing error:",error);
-        throw new Error("Failed to extract text fro PDF" )  
-    }
-}
+    return {
+      text: data.text,
+      numPages: data.numpages,
+      info: data.info,
+    };
+  } catch (error) {
+    console.error("PDF parsing error:", error);
+    throw new Error("Failed to extract text from PDF");
+  }
+};
